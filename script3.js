ae63653aca98dc7e95dab1ee5228a88110bfe340
Yesenia-da <yeseniamicanquer760@gmail.com>
2024-11-13 17:33:58 -0500

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,4 @@
+let base=parseInt(prompt("ingresa la base del triángulo"));
+let altura=parseInt(prompt("ingresa la altura del triángulo"));
+let area=(base*altura) / 2 ;
+document.write("El area del triangulo es"  + area)
