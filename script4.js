069b0ed4abd4424987921f6af8101a0c570c0e45
Yesenia-da <yeseniamicanquer760@gmail.com>
2024-11-13 17:34:38 -0500

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,4 @@
+let numero=parseInt(prompt("ingredar un numero:"));
+let exponente=parseInt(prompt("ingresar el exponente"));
+let resultado=(base^exponente);
+document.write("el resultado obtenido es:" + resultado)
