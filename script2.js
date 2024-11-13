d789da722d1018624ade0a9295938cf3c21e5cb8
Yesenia-da <yeseniamicanquer760@gmail.com>
2024-11-13 17:33:18 -0500

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,5 @@
+let nota1 =parseInt(prompt("Ingresa la nota del primer período:"));
+let nota2 =parseInt (prompt("Ingresa la nota del segundo período:"));
+let nota3 =parseInt (prompt("Ingresa la nota del tercer período:"));
+let promedio = (parseInt (nota1) + parseInt(nota2) + parseInt (nota3)) / 3;
+console.log("El promedio es: " + promedio);
